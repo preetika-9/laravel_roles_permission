a6bcb29c75214ff2cef9b21cfd2d8ca363a2118e
pritika <66292568+preetika-9@users.noreply.github.com>
2024-07-23 21:39:40 +0545

    role crud

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,21 +3,114 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
     //this will show roles page
     public function index()
     {
+        $roles = Role::orderBy('name', 'ASC')->paginate(10);
+        return view('roles.list', [
+            'roles' => $roles
+        ]);
     }
 
     //this will create roles page
     public function create()
     {
+        $permissions = Permission::orderBy('name', 'ASC')->get();
+        return view('roles.create', [
+            'permissions' =>  $permissions
+        ]);
     }
 
     //this will insertI roles page
-    public function store()
+    public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:roles|min:3'
+        ]);
+
+        if ($validator->passes()) {
+            // dd($request->permission);
+            $role =  Role::create(['name' => $request->name]);
+
+
+            if (!empty($request->permission)) {
+                foreach ($request->permission as $name) {
+                    $role->givePermissionTo(($name));
+                }
+            }
+
+            return redirect()->route('roles.index')->with('success', 'Role added successfully');
+        } else {
+            return redirect()->route('roles.create')->withInput()->withErrors($validator);
+        }
+    }
+
+    // edit
+    public function edit($id)
+    {
+        $role = Role::findOrFail($id);
+        $hasPermissions = $role->permissions->pluck('name');
+        $permissions = Permission::orderBy('name', 'ASC')->get();
+
+        return view('roles.edit', [
+            'permissions' => $permissions,
+            'hasPermissions' => $hasPermissions,
+            'role' => $role
+        ]);
+    }
+
+    //?update
+    public function update($id, Request $request)
+    {
+        $role = Role::findOrFail($id);
+        // dd($role);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:roles,name,' . $id . ',id'
+        ]);
+
+        if ($validator->passes()) {
+            // dd($request->permission);
+            // $role =  Role::create(['name' => $request->name]);
+            $role->name = $request->name;
+            $role->save();
+
+
+            if (!empty($request->permission)) {
+                $role->syncPermissions($request->permission);
+            } else {
+                $role->syncPermissions([]);
+            }
+
+            return redirect()->route('roles.index')->with('success', 'Role updates successfully');
+        } else {
+            return redirect()->route('roles.edit')->withInput()->withErrors($validator);
+        }
+    }
+
+    public function destroy(Request $request)
+
+    {
+        $id = $request->id;
+        $role = Role::find($id);
+
+        if ($role == null) {
+            session()->flash('error', 'Role not found');
+            return response()->json([
+                'status' => false
+            ]);
+        }
+
+        $role->delete();
+
+        session()->flash('success', 'Role deleted successfully');
+        return response()->json([
+            'status' => true
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,13 +18,22 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-
+    // permission routes
     Route::get('/permissions', [PermissionController::class, 'index'])->name('permissions.index');
     Route::get('/permissions/create', [PermissionController::class, 'create'])->name('permissions.create');
     Route::post('/permissions', [PermissionController::class, 'store'])->name('permissions.store');
     Route::get('/permissions/{id}/edit', [PermissionController::class, 'edit'])->name('permissions.edit');
     Route::post('/permissions/{id}', [PermissionController::class, 'update'])->name('permissions.update');
     Route::delete('/permissions', [PermissionController::class, 'destroy'])->name('permissions.destroy');
+
+    // roles
+    Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
+    Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
+    Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
+    Route::get('/roles/{id}/edit', [RoleController::class, 'edit'])->name('roles.edit');
+    Route::post('/roles/{id}', [RoleController::class, 'update'])->name('roles.update');
+    Route::delete('/roles', [RoleController::class, 'destroy'])->name('roles.destroy');
 });
+
 
 require __DIR__ . '/auth.php';
