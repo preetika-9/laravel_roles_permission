6dfcc635749305dadc04afcfe96caa1f96b1411e
pritika <66292568+preetika-9@users.noreply.github.com>
2024-07-30 21:44:10 +0545

    user

---
+++ resources/views/users/list.blade.php
@@ -0,0 +1,84 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Users') }}
+            </h2>
+            <a href="{{ route('roles.create') }}" class="bg-slate-700 text-sm rounded-md px-3 py-2 text-white">Create</a>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <x-message></x-message>
+
+
+            <table class="w-full">
+                <thead class="bg-gray-50">
+                    <tr class="border-b">
+                        <th class="px-6 py-3 text-left" width="60">#</th>
+                        <th class="px-6 py-3 text-left">Name</th>
+                        <th class="px-6 py-3 text-left">Email</th>
+                        <th class="px-6 py-3 text-left">Roles</th>
+                        <th class="px-6 py-3 text-left" width="150">Created</th>
+                        <th class="px-6 py-3 text-center" width="180">Action</th>
+                    </tr>
+                </thead>
+
+                <tbody class="bg-white">
+                    @if ($users->isNotEmpty())
+                        @foreach ($users as $user)
+                            <tr class="border-b">
+                                <td class="px-6 py-3 text-left">{{ $user->id }}</td>
+                                <td class="px-6 py-3 text-left">{{ $user->name }}</td>
+                                <td class="px-6 py-3 text-left">{{ $user->email }}
+                                </td>
+                                <td class="px-6 py-3 text-left">{{ $user->roles->pluck('name')->implode(', ') }}
+                                </td>
+                                <td class="px-6 py-3 text-left">
+                                    {{ \Carbon\Carbon::parse($user->created_at)->format('d M, Y') }}</td>
+                                <td class="px-6 py-3 text-center">
+
+                                    <a href="{{ route('users.edit', $user->id) }}"
+                                        class="bg-slate-700 text-sm rounded-md text-white px-3 py-2 hover:bg-slate-600">Edit</a>
+                                    {{--    <a href="javascript:void(0);" onclick="deleteRole({{ $user->id }})"
+                                        class="bg-red-700 text-sm rounded-md text-white px-3 py-2 hover:bg-red-600">Delete</a> --}}
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    @endif
+
+                </tbody>
+            </table>
+
+            <div class="my-3">
+                {{ $users->links() }}
+            </div>
+        </div>
+    </div>
+
+    <x-slot name="script">
+        <script type="text/javascript">
+            function deleteRole(id) {
+                if (confirm("Are you sure want to delete?")) {
+                    $.ajax({
+                        url: '{{ route('roles.destroy') }}',
+                        type: 'delete',
+                        data: {
+                            id: id
+                        },
+                        dataType: 'json',
+                        headers: {
+                            'x-csrf-token': '{{ csrf_token() }}'
+                        },
+                        success: function(response) {
+                            window.location.href = '{{ route('roles.index') }}'
+                        }
+
+                    })
+                }
+            }
+        </script>
+    </x-slot>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -42,6 +43,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/articles/{id}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
     Route::post('/articles/{id}', [ArticleController::class, 'update'])->name('articles.update');
     Route::delete('/articles', [ArticleController::class, 'destroy'])->name('articles.destroy');
+
+    // user
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    // Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    // Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::post('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    // Route::delete('/articles', [ArticleController::class, 'destroy'])->name('articles.destroy');
+
 });
 
 
